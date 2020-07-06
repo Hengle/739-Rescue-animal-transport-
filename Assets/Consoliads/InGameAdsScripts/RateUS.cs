bc9f4058b80b7ceb3dbc8761be26975f30413c5b
RanaUzairThakur <66404667+RanaUzairThakur@users.noreply.github.com>
2020-07-06 03:42:18 -0700

    some

---
+++ Assets/Consoliads/InGameAdsScripts/RateUS.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RateUS : MonoBehaviour {
+
+	Button RateUSButton;
+
+	void Start () 
+	{
+		RateUSButton = this.GetComponent<Button> () as Button;
+		RateUSButton.onClick.AddListener (GaminatorAds.Instance.RateUs);
+	}
+
+
+}
